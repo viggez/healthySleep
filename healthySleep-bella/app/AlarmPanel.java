d9178e68fb4609d6bedfd643dee13b96141cb2c8
babis0301 <bella.j.g@hotmail.com>
2023-05-04 14:04:42 +0200

    Added RelaxingSound Class and SleepEnviormentClass. Fixed bugs.
    Added new classes to the calm down feature, started working on the Sleep Environment class and made changes to main and AlarmPanel.
    Fixed bug in AlarmPanel and method playAlarmSound.

---
+++ healthySleep-bella/app/AlarmPanel.java
@@ -0,0 +1,88 @@
+package app;
+
+
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.time.DateTimeException;
+import java.time.LocalTime;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+
+public class AlarmPanel extends JPanel {
+    private static final long serialVersionUID = 1L;
+    public final JTextField hourField = new JTextField(2);
+    public final JTextField minuteField = new JTextField(2);
+    public final JTextField secondField = new JTextField(2);
+    private final JButton alarmButton;
+    private boolean alarmActive = false;
+    private LocalTime alarmTime = null;
+
+
+    public AlarmPanel() {
+        this.setLayout(new GridBagLayout());
+        this.setPreferredSize(new Dimension(650, 150));
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = 2;
+        this.alarmButton = new JButton("Set Alarm");
+        this.alarmButton.addActionListener((e) -> {
+            this.toggleAlarm();
+        });
+        c.gridx = 0;
+        c.gridy = 0;
+        this.add(new JLabel("Hour:"), c);
+        c.gridx = 1;
+        c.gridy = 0;
+        this.add(this.hourField, c);
+        c.gridx = 2;
+        c.gridy = 0;
+        this.add(new JLabel("Minute:"), c);
+        c.gridx = 3;
+        c.gridy = 0;
+        this.add(this.minuteField, c);
+        c.gridx = 4;
+        c.gridy = 0;
+        this.add(new JLabel("Second:"), c);
+        c.gridx = 5;
+        c.gridy = 0;
+        this.add(this.secondField, c);
+        c.gridx = 6;
+        c.gridy = 0;
+        this.add(this.alarmButton, c);
+    }
+
+
+    public void toggleAlarm() {
+        if (!this.alarmActive) {
+            try {
+                int hour = Integer.parseInt(this.hourField.getText());
+                int minute = Integer.parseInt(this.minuteField.getText());
+                int second = Integer.parseInt(this.secondField.getText());
+                this.alarmTime = LocalTime.of(hour, minute, second);
+                this.alarmActive = true;
+                this.alarmButton.setText("Cancel Alarm");
+            } catch (DateTimeException | NumberFormatException var4) {
+                JOptionPane.showMessageDialog(this, "Invalid time format. Please enter valid values.", "Error", 0);
+            }
+        } else {
+            this.alarmActive = false;
+            this.alarmButton.setText("Set Alarm");
+        }
+
+
+    }
+
+
+    public boolean isAlarmActive() {
+        return this.alarmActive;
+    }
+
+
+    public LocalTime getAlarmTime() {
+        return this.alarmTime;
+    }
+}
---
+++ healthySleep-bella/app/RelaxingSounds.java
@@ -0,0 +1,116 @@
+package app;
+
+
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+
+public class RelaxingSounds extends JPanel implements ActionListener {
+    private JComboBox<String> comboBox;
+    private JButton startButton;
+    private JButton stopButton;
+    private JLabel label = new JLabel("Pick a sound: ");
+    Instant endTime;
+    Clip clip;
+
+
+    public RelaxingSounds() {
+        this.label.setFont(new Font("Arial", 1, 20));
+        this.setBounds(100, 300, 100, 30);
+        this.label.setOpaque(true);
+        this.add(this.label);
+        String[] soundOptions = new String[]{"Rain and thunderstorm", "Rain on window", "Relaxing piano"};
+        this.comboBox = new JComboBox(soundOptions);
+        this.setBounds(70, 300, 300, 30);
+        this.comboBox.addActionListener(this);
+        this.add(this.comboBox);
+        this.startButton = new JButton("Play sound");
+        this.startButton.setFocusable(false);
+        this.setBounds(50, 20, 30, 30);
+        this.startButton.addActionListener(this);
+        this.add(this.startButton);
+        this.stopButton = new JButton("Stop sound");
+        this.stopButton.setFocusable(false);
+        this.stopButton.addActionListener(this);
+        this.stopButton.setEnabled(false);
+        this.add(this.stopButton);
+    }
+
+
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == this.comboBox) {
+            if (this.clip != null && this.clip.isRunning()) {
+                this.clip.stop();
+            }
+
+
+            this.startButton.setEnabled(true);
+        } else if (e.getSource() == this.startButton) {
+            String selectedSound = (String)this.comboBox.getSelectedItem();
+            if (selectedSound != null) {
+                try {
+                    File audioFile = new File(this.getAudioFilePath(selectedSound));
+                    AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
+                    AudioFormat format = audioStream.getFormat();
+                    DataLine.Info info = new DataLine.Info(Clip.class, format);
+                    this.clip = (Clip)AudioSystem.getLine(info);
+                    this.clip.open(audioStream);
+                    this.clip.loop(-1);
+                    this.endTime = Instant.now().plus(3L, ChronoUnit.HOURS);
+                    this.startButton.setEnabled(false);
+                    this.stopButton.setEnabled(true);
+                } catch (UnsupportedAudioFileException | LineUnavailableException | IOException var7) {
+                    var7.printStackTrace();
+                }
+            }
+        } else if (e.getSource() == this.stopButton) {
+            this.stopAudio();
+        }
+
+
+    }
+
+
+    private void stopAudio() {
+        if (this.clip != null && this.clip.isRunning()) {
+            this.clip.stop();
+            this.startButton.setEnabled(true);
+            this.stopButton.setEnabled(false);
+        }
+
+    }
+
+    private String getAudioFilePath(String soundName) {
+        String filePath = "";
+        switch (soundName) {
+            case "Rain and thunderstorm":
+                filePath = "rain-and-thunder-nature-sounds-7803.wav";
+                break;
+            case "Rain on window":
+                filePath = "rain-on-roof-or-window-nature-sounds-8312.wav";
+                break;
+            case "Relaxing piano":
+                filePath = "the-last-piano-112677.wav";
+        }
+
+
+        return filePath;
+    }
+}
+

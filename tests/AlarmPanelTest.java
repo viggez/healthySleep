fc02c2571b6e2d9dd65187785cd647a3a0173b32
babis0301 <bella.j.g@hotmail.com>
2023-05-04 15:15:08 +0200

    Put AlarmPanel back
    Deleted it from my commit, because I couldn't run the application on my IntelliJ and forgot to add it back.

---
+++ tests/AlarmPanelTest.java
@@ -0,0 +1,50 @@
+@@ -1,49 +0,0 @@
+        package tests;
+
+        import org.junit.jupiter.api.BeforeEach;
+        import org.junit.jupiter.api.Test;
+
+        import app.AlarmPanel;
+
+        import java.time.LocalTime;
+        import static org.junit.jupiter.api.Assertions.*;
+
+class AlarmPanelTest {
+
+    private AlarmPanel alarmPanel;
+
+    @BeforeEach
+    void setUp() {
+        alarmPanel = new AlarmPanel();
+    }
+
+    @Test
+    void testToggleAlarm() {
+        // Test that the alarm is initially inactive
+        assertFalse(alarmPanel.isAlarmActive());
+
+        // Set a valid alarm time
+        alarmPanel.hourField.setText("12");
+        alarmPanel.minuteField.setText("00");
+        alarmPanel.secondField.setText("00");
+
+        // Test that the alarm becomes active after toggling
+        alarmPanel.toggleAlarm();
+        assertTrue(alarmPanel.isAlarmActive());
+
+        // Test that the alarm becomes inactive after toggling again
+        alarmPanel.toggleAlarm();
+        assertFalse(alarmPanel.isAlarmActive());
+    }
+
+    @Test
+    void testValidAlarmTime() {
+        alarmPanel.hourField.setText("12");
+        alarmPanel.minuteField.setText("00");
+        alarmPanel.secondField.setText("00");
+        alarmPanel.toggleAlarm();
+
+        LocalTime expectedAlarmTime = LocalTime.of(12, 0, 0);
+        assertEquals(expectedAlarmTime, alarmPanel.getAlarmTime());
+    }
+}
